59ae77e84fe2f40e0e3d7089aedcb4c427c1cc36
GitPoomCmu <152594220+GitPoomCmu@users.noreply.github.com>
2023-12-26 16:13:43 +0700

    Update lab12_4.cpp

--- lab12_4.cpp
+++ lab12_4.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
-
 using namespace std;
+
+template <class input>
+void mySwap(input &, input &);
 
 int main(){
 	int x, y;
@@ -30,3 +32,9 @@ int main(){
 	return 0;
 }
 
+template <class input>
+void mySwap(input &x, input &y){
+    input z = x;
+    x = y;
+    y = z;
+}
